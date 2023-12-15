d99ed5a7a8f2dff71ab199f4a18463832333b73a
Olga Pedashenko <olga.pedashenko@gmail.com>
2023-12-15 19:36:45 +0100

    feat: ad d some solutions

--- basic-js-ds/src/delete-digit.js
+++ basic-js-ds/src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(num) {
+  const array = String(num).split("").map(Number);
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < array[i + 1]) {
+      array.splice(i, 1);
+      return Number(array.join(""));
+    }
+  }
+  array.pop();
+  return Number(array.join(""));
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- basic-js-ds/src/dns-stats.js
+++ basic-js-ds/src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const obj = {};
+  domains.forEach((domain) => {
+    const elements = domain.split(".").reverse();
+    let name = "";
+
+    elements.forEach((element) => {
+      name += `.${element}`;
+      obj[name] = (obj[name] || 0) + 1;
+    });
+  });
+  return obj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
